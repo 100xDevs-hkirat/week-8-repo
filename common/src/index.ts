fb48e37d95b11a774d4f03e88f4e02fd8e6b46ff
Harkirat Singh <harkirat96@gmail.com>
2023-07-30 19:57:52 +0530

    added module

--- common/src/index.ts
+++ common/src/index.ts
@@ -3,6 +3,6 @@ export const signupInput = z.object({
     username: z.string(),
     password: z.string()
 })
+console.log("hi there");
 
 export type SignupParams = z.infer<typeof signupInput>;
-  
--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -2,12 +2,21 @@ import jwt from "jsonwebtoken";
 import express from 'express';
 import { authenticateJwt, SECRET } from "../middleware/";
 import { User } from "../db";
+import { signupInput } from "@100xdevs/common"
 
 const router = express.Router();
 
-  router.post('/signup', async (req, res) => {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
+router.post('/signup', async (req, res) => {
+    let parsedInput = signupInput.safeParse(req.body)
+    if (!parsedInput.success) {
+      return res.status(403).json({
+        msg: "error"
+      });
+    }
+    const username = parsedInput.data.username 
+    const password = parsedInput.data.password 
+    
+    const user = await User.findOne({ username: parsedInput.data.username });
     if (user) {
       res.status(403).json({ message: 'User already exists' });
     } else {
